2ead96432bfe452f2f1aabdabe814463999eeab5
Agrim-Bansal <agrimx2@gmail.com>
2020-09-05 19:18:51 +0530

    Menu Bar added but links left

---
+++ menubar.js
@@ -0,0 +1,17 @@
+
+
+var m1 = document.getElementById("m1")
+m1.onmouseover = function () { m1.style.backgroundImage = 'url("menubg.jpg")'};
+m1.onmouseout = function () { m1.style.backgroundImage = "none" };
+
+var m2 = document.getElementById("m2")
+m2.onmouseover = function () { m2.style.backgroundImage = 'url("menubg.jpg")' };
+m2.onmouseout = function () { m2.style.backgroundImage = "none" };
+
+var m3 = document.getElementById("m3")
+m3.onmouseover = function () { m3.style.backgroundImage = 'url("menubg.jpg")' };
+m3.onmouseout = function () { m3.style.backgroundImage = "none" };
+
+var m4 = document.getElementById("m4")
+m4.onmouseover = function () { m4.style.backgroundImage = 'url("menubg.jpg")' };
+m4.onmouseout = function () { m4.style.backgroundImage = "none" };
