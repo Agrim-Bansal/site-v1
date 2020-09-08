55224c4684bedd8b92c2cf74475f37a0970a25d8
Agrim-Bansal <agrimx2@gmail.com>
2020-09-08 10:59:01 +0530

    lil changes

--- menubar.js
+++ menubar.js
@@ -1,17 +1,21 @@
 
 
-var m1 = document.getElementById("m1")
+var m1 = document.getElementById("m1");
 m1.onmouseover = function () { m1.style.backgroundImage = 'url("menubg.jpg")'};
 m1.onmouseout = function () { m1.style.backgroundImage = "none" };
 
-var m2 = document.getElementById("m2")
+var m2 = document.getElementById("m2");
 m2.onmouseover = function () { m2.style.backgroundImage = 'url("menubg.jpg")' };
 m2.onmouseout = function () { m2.style.backgroundImage = "none" };
 
-var m3 = document.getElementById("m3")
+var m3 = document.getElementById("m3");
 m3.onmouseover = function () { m3.style.backgroundImage = 'url("menubg.jpg")' };
 m3.onmouseout = function () { m3.style.backgroundImage = "none" };
 
-var m4 = document.getElementById("m4")
+var m4 = document.getElementById("m4");
 m4.onmouseover = function () { m4.style.backgroundImage = 'url("menubg.jpg")' };
 m4.onmouseout = function () { m4.style.backgroundImage = "none" };
+
+var m5 = document.getElementById("m5");
+m5.onmouseover = function () { m5.style.backgroundImage = "url('menubg.jpg')"};
+m5.onmouseout = function () { m5.style.backgroundImage = "none" };
